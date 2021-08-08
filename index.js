d5c02d705f1022aace55e7c3e127b733a6eeb055
richard <richardyu042@gmail.com>
2021-08-07 23:36:34 -0400

    added timing and post logic

--- index.js
+++ index.js
@@ -6,33 +6,50 @@ var options = {
     ignoreNameSpace : true
 }
 
+var time 
+const postLink
+const getLink = "https://reddit.com/r/animereactionimages/new.rss" 
+
 function main(){
-    var entries = getEntries();
-    for(entry in entries){
-      console.log(buildWebhookPost(entry))
+  startup()
+  while(true){
+    var posts = updateLoop();
+    for(p in posts.reverse()){
+      postNewEntry(p, link)
     }
+  }
+}
+
+function startup(){
+  time = Date.now()
+
+}
+
+function updateLoop(){
+  var entries = getEntries();
+  var posts = []
+  for (entry in entries) {
+    if(Date.parse(entry.published) > time){
+      console.log(buildWebhookPost(entry))
+      posts.push(buildWebhookPost(entry))
+    }
+  }
+  return posts
 }
 
 async function getEntries() {
-  const buffer = await got("https://reddit.com/r/animereactionimages/new.rss", {
+  const buffer = await got(getLink, {
     responseType: "buffer",
     resolveBodyOnly: true,
     timeout: 5000,
     retry: 5,
   });
   var feed = parser.parse(buffer.toString(), options);
-  for (const item of feed.feed.entry) {
+  //for (const item of feed.feed.entry) {
     //console.log({ title: item.title, url: item.link, thumb: item.thumbnail });
-  }
+  //}
   return feed.feed.entry
 };
-
-function findNew(entries){
-  for(const item of entries){
-    //docker images here to cache last x entries so we can only send new stuff/
-    //dont forget to handle deleted stuff
-  }
-}
 
 function buildWebhookPost(entry){
   var ret = {}
@@ -50,9 +67,9 @@ function buildWebhookPost(entry){
   return JSON.stringify(ret)
 }
 
-function postNewEntry(title, postLink, imageUrl){
+function postNewEntry(content, postLink){
   var request = new XMLHttpRequest();
   request.open('POST', postLink, true);
-  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-  request.send(title);
+  request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+  request.send(content);
 }
