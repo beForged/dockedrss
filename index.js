22e37d224d01abd14ee20389a51bd6e6b841e6ae
richard <richardyu042@gmail.com>
2021-08-13 19:12:07 -0400

    actually works

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 require('dotenv').config()
 const got = require("got");
 const parser = require("fast-xml-parser");
+const express = require("express")
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 
 var options = {
     ignoreAttributes : false,
+    attributeNamePrefix : "_",
     ignoreNameSpace : true
 }
+
+const PORT = process.env.PORT || 3000;
+const app = express()
+app.listen(PORT, () => {
+    console.log(`app is running on port ${ PORT }`);
+});
 
 var time 
 const ONE_MINUTE = 1000 * 60
@@ -16,63 +26,71 @@ main()
 function main(){
   console.log("running main")
   time = Date.now()
-  setInterval(fetchAndPost, ONE_MINUTE)
-}
-
-function fetchAndPost(){
-    var posts = getNewEntries();
-    //goes backwards for time keeping reasons
-    for(p in posts.reverse()){
-      postNewEntry(p, process.env.WEBHOOK_LINK)
-      time = Date.parse(p.entry.published)
-    }
-}
-
-function getNewEntries(){
-  var entries = getAllEntries();
-  var posts = []
-  for (entry in entries) {
-    if(Date.parse(entry.published) > time){
-      console.log("new entry found")
-      console.log(buildWebhookPost(entry))
-      posts.push(buildWebhookPost(entry))
-    }
-  }
-  return posts
+  //inital
+  getAllEntries()
+  //setInterval(getAllEntries, ONE_MINUTE)
 }
 
 async function getAllEntries() {
   console.log("fetching rss feed")
-  const buffer = await got(process.env.FETCH_LINK, {
-    responseType: "buffer",
-    resolveBodyOnly: true,
-    timeout: 5000,
-    retry: 5,
-  });
-  var feed = parser.parse(buffer.toString(), options);
-  //for (const item of feed.feed.entry) {
-    //console.log({ title: item.title, url: item.link, thumb: item.thumbnail });
-  //}
-  return feed.feed.entry
+  try {
+    const buffer = await got(process.env.FETCH_LINK, {
+      responseType: "buffer",
+      resolveBodyOnly: true,
+      timeout: 5000,
+      retry: 5,
+    });
+    console.log("response recieved")
+
+    var feed = parser.parse(buffer.toString(), options);
+    console.log("parsed feed")
+  } catch (error) {
+    console.log(error)
+  }
+
+  for (const item of feed.feed.entry) {
+    //console.log({ title: item.title, url: item.link, thumb: item.thumbnail , pub: item.published});
+    //console.log({name: item.author.name})
+  }
+
+  var posts = []
+  for (const entry of feed.feed.entry) {
+    if (Date.parse(entry.published) > (time)) { 
+      //console.log({title: entry.title})
+      //console.log(buildWebhookPost({entry}))
+      posts.push({entry})
+    }
+  }
+
+  //goes backwards for time keeping reasons
+  for (p in posts.reverse()) {
+    var ret = buildWebhookPost(posts[p])
+    postNewEntry(ret, process.env.WEBHOOK_LINK)
+    //console.log("updated time: " + posts[p].entry.published)
+    time = Date.parse(posts[p].entry.published)
+  }
 };
 
-function buildWebhookPost(entry){
+function buildWebhookPost(entry) {
   var ret = {}
   var embeds = {}
-  embeds.title = entry.title
-  embeds.url = entry.link
+  embeds.title = entry.entry.title
+  embeds.url = entry.entry.link._href
+  const url = entry.entry.thumbnail._url
   embeds.image = {
-    url: entry.item.thumbnail
+    url: url.replace(/amp;/g, '')
   }
   embeds.author = {
-    name: entry.author.name,
-    url: entry.author.uri
+    name: entry.entry.author.name,
+    url: entry.entry.author.uri
   }
-  ret.embeds = embeds
+  ret.embeds = []
+  ret.embeds.push(embeds)
+  //console.log(JSON.stringify(ret))
   return JSON.stringify(ret)
 }
 
-function postNewEntry(content, postLink){
+function postNewEntry(content, postLink) {
   var request = new XMLHttpRequest();
   request.open('POST', postLink, true);
   request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
